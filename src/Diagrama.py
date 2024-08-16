ec2ca488c72e464e1830c27235f6a9c527e3b7dd
rapharias <exemplo@gmail.com>
2024-08-16 20:14:03 -0300

    crate TF

--- src/Diagrama.py
+++ src/Diagrama.py
@@ -1,28 +1,11 @@
-from diagrams import Cluster, Diagram
-from diagrams.aws.compute import Lambda
-from diagrams.aws.database import ElastiCache, RDS
-from diagrams.aws.network import ELB, Route53, APIGateway
-from diagrams.aws.management  import Cloudwatch
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
 
-with Diagram("Clustered Web Services with Logs", show=False):
-    dns = Route53("dns")
-    lb = ELB("lb")
-    api_gateway = APIGateway("API Gateway")
-
-    with Cluster("Services"):
-        lambda_group = [Lambda("web1"),
-                        Lambda("web2"),
-                        Lambda("web3")]
-
-    with Cluster("DB Cluster"):
-        db_primary = RDS("userdb")
-        db_primary - [RDS("userdb ro")]
-
-    memcached = ElastiCache("memcached")
-    logs = Cloudwatch("logs")
-
-    dns >> lb >> api_gateway >> lambda_group
-    lambda_group >> db_primary
-    lambda_group >> memcached
-    lambda_group >> logs
-    api_gateway >> logs
+with Diagram("Grouped Workers", show=False, direction="TB"):
+    ELB("lb") >> [EC2("worker1"),
+                  EC2("worker2"),
+                  EC2("worker3"),
+                  EC2("worker4"),
+                  EC2("worker5")] >> RDS("events")
